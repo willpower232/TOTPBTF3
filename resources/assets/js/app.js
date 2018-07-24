d07545a8c7a854ba77d0bab7fcb5b295057ed43e
William Hall <github@willpoweredinc.co.uk>
2018-07-24 08:04:35 +0100

    mix and theme

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,52 @@
-// todo
+(function() {
+	if (typeof window.refreshat !== 'undefined') {
+		var loop = setInterval(function() {
+			if (window.refreshat < (Date.now() / 1000)) {
+				// now is the time, stop ticking
+				clearInterval(loop);
+
+				// bump the refresh into the future to avoid double refresh
+				setTimeout(function() {
+					window.location.reload();
+				}, 500);
+			}
+		}, 1000);
+	}
+
+	if ((toggle = document.querySelector('input[name="light_mode"]'))) {
+		toggle.addEventListener('change', function(ev) {
+			var xhr = new XMLHttpRequest(),
+				data = new FormData();
+
+			data.append('light_mode', toggle.checked);
+
+			xhr.open('POST', '/api/profile/setLightMode');
+			xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+			xhr.setRequestHeader('Accept', 'application/json');
+
+			xhr.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+
+			xhr.onload = function() {
+				var output = this.responseText;
+
+				if (this.status == 200) {
+					try {
+						output = JSON.parse(output);
+
+						if (output.current_state === true) {
+							document.body.parentNode.classList.add('inverted');
+						} else if (output.current_state === false) {
+							document.body.parentNode.classList.remove('inverted');
+						}
+					} catch (ex) {
+						alert('Not received JSON');
+					}
+				} else {
+					alert('Received: ' + this.status);
+				}
+			};
+
+			xhr.send(data);
+		});
+	}
+})();
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,21 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// using scripts instead of js to avoid webpack bloating plain js - must specify full path to output file
+mix.scripts('resources/assets/js/app.js', 'public/js/app.js');
+
+// standard scss compile
+mix.sass('resources/assets/sass/app.scss', 'public/css').options({
+	autoprefixer: false
+});
+
+// auto cache bust
+mix.version();
+
+// sourcemaps only happen if minified i.e. prod
+// mix.sourceMaps();
+
+// disable notifications if building on server
+// if (mix.inProduction()) {
+//     mix.disableNotifications();
+// }
