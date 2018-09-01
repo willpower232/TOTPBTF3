de272a69a62ae3d3df075cd128821bee1b10de97
William Hall <github@willpoweredinc.co.uk>
2018-09-01 12:55:01 +0100

    export mysql database to sqlite

---
+++ app/Console/Commands/MakeSqliteBackup.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class MakeSqliteBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'makesqlitebackup';
+
+    /**
+     * Create SQLite backup of MySQL database.
+     *
+     * This requires the mysqldump command from the mysql-client package and the sqlite3 package.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        unlink(database_path(env('DB_SQLITE_DATABASE', 'database.sqlite')));
+
+        $process = new Process(sprintf('sh mysql2sqlite.sh -h %s -u %s -p%s %s | sqlite3 %s', env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'), env('DB_DATABASE'), database_path(env('DB_SQLITE_DATABASE', 'database.sqlite'))));
+
+        // $process->start();
+
+        // foreach ($process as $type => $data) {
+        //     if ($process::OUT === $type) {
+        //         echo "\nRead from stdout: ".$data;
+        //     } else { // $process::ERR === $type
+        //         echo "\nRead from stderr: ".$data;
+        //     }
+        // }
+
+        $process->run();
+
+        if (! $process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        // I have no idea why this says memory
+        echo $process->getOutput();
+    }
+}
