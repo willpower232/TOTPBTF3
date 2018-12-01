fe9f11ad0bad00d75ec826d222406ad7f86a2b32
William Hall <github@willpoweredinc.co.uk>
2018-12-01 13:30:52 +0000

    test hashids wrapper

---
+++ tests/Feature/Helpers/HashidsTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Tests\Feature\Helpers;
+
+use Tests\TestCase;
+use App\Helpers\Hashids;
+use Exception;
+
+class HashidsTest extends TestCase
+{
+    private static $hashidssalt = 'lsngmym1nd';
+
+    /**
+     * Prepare for using hashids helper with known salt.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        // alter env before running setup
+        putenv('HASHIDS_SALT=' . self::$hashidssalt);
+
+        parent::setUp();
+    }
+
+    /**
+     * Tests that our Hashids wrapper is capable of decoding what it encodes
+     *
+     * @return void
+     */
+    public function testDecodeEncode()
+    {
+        $number = 74656;
+        $this->assertEquals($number, Hashids::decode(Hashids::encode($number)));
+    }
+
+    /**
+     * Tests that our Hashids wrapper will throw exception when decoding an invalid value
+     *
+     * @return void
+     */
+    public function testDecodeFailure()
+    {
+        $number = 74656;
+        $string = Hashids::encode($number);
+        $string .= 'F';
+
+        $this->expectException(Exception::class);
+        $output = Hashids::decode($string);
+    }
+
+    /**
+     * Tests that our Hashids wrapper will throw exception when encoding an invalid value
+     *
+     * @return void
+     */
+    public function testEncodeFailure()
+    {
+        $string = 'tell me I am fine';
+
+        $this->expectException(Exception::class);
+        $output = Hashids::encode($string);
+    }
+}
