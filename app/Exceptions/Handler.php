cbc57e5e3c62ca60e1e0befade6fceba843ee200
William Hall <github@willpoweredinc.co.uk>
2019-01-05 12:23:47 +0000

    turns out this code was unnecessary and request can't be mocked easily to test exceptions(?)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,54 +25,4 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
-
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        // https://stackoverflow.com/a/34791686
-        if ($this->isHttpException($exception) && method_exists($exception, 'getStatusCode')) {
-            switch ($exception->getStatusCode()) {
-                // not authorized
-                case '403':
-                    return response()->view('error', array('errormessage' => 403), 403);
-                    break;
-
-                // not found
-                case '404':
-                    return response()->view('error', array('errormessage' => 404), 404);
-                    break;
-
-                // method not allowed
-                case '405': // $exception instanceof MethodNotAllowedHttpException
-                    return response()->view('error', array('errormessage' => 405), 405);
-                    break;
-
-                // internal error
-                case '500':
-                    return response()->view('error', array('errormessage' => 500), 500);
-                    break;
-            }
-
-            return $this->renderHttpException($exception);
-        }
-
-        return parent::render($request, $exception);
-    }
 }
