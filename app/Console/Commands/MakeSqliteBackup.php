cf0a890b4e67b4eab92f9b582eb7ba6561a2dfe0
William Hall <github@willpoweredinc.co.uk>
2019-01-05 13:35:59 +0000

    drastically improve the sqlite backup command

--- app/Console/Commands/MakeSqliteBackup.php
+++ app/Console/Commands/MakeSqliteBackup.php
@@ -15,7 +15,7 @@ class MakeSqliteBackup extends Command
     protected $signature = 'makesqlitebackup';
 
     /**
-     * Create SQLite backup of MySQL database. Too difficult to trigger the exception to include in code coverage
+     * Create SQLite backup of MySQL database.
      *
      * This requires the mysqldump command from the mysql-client package and the sqlite3 package.
      *
@@ -26,9 +26,10 @@ class MakeSqliteBackup extends Command
     public function handle()
     {
         $databasefile = config('database.connections.sqlite.database');
-        if (file_exists($databasefile)) {
-            unlink($databasefile);
-        }
+
+        // replacing the file would upset users if they weren't using change management
+        // so create a new file first
+        $newdatabasefile = substr_replace($databasefile, '_new.sqlite', '-7');
 
         $process = new Process(sprintf(
             'sh mysql2sqlite.sh -h %s -u %s -p%s %s | sqlite3 %s',
@@ -36,26 +37,37 @@ class MakeSqliteBackup extends Command
             config('database.connections.mysql.username'),
             config('database.connections.mysql.password'),
             config('database.connections.mysql.database'),
-            $databasefile
+            $newdatabasefile
         ));
-
-        // $process->start();
-
-        // foreach ($process as $type => $data) {
-        //     if ($process::OUT === $type) {
-        //         echo "\nRead from stdout: ".$data;
-        //     } else { // $process::ERR === $type
-        //         echo "\nRead from stderr: ".$data;
-        //     }
-        // }
 
         $process->run();
 
-        if (! $process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+        // since the command pipes the output to sqlite3,
+        // we will only get a non-zero exit code if sqlite3 fails,
+        // i.e. the path to the file is wrong or the sqlite is badly generated
+        // we will not get a non-zero exit code if mysqldump fails!
+
+        // in order to detect any failed part of the process, we have to detect the presence of error text
+        $erroroutput = $process->getErrorOutput();
+
+        if (strlen($erroroutput) > 0) {
+            unlink($newdatabasefile);
+            $this->error(trim($erroroutput));
+            return 1;
         }
 
-        // I have no idea why this says memory
-        echo $process->getOutput();
+        // todo: determine if its possible for the command to succeed and generate an empty file
+
+        // this classic Symfony Process code is less useful than the above
+        // if (! $process->isSuccessful()) {
+        //     throw new ProcessFailedException($process);
+        // }
+
+        rename($newdatabasefile, $databasefile);
+        $this->info('Database download and conversion successful');
+        return 0;
+
+        // I still have no idea why this says memory
+        // echo $process->getOutput();
     }
 }
