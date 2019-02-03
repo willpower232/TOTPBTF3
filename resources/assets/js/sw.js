c9a09db4c28b0804bdf41e64faeec66cf6fcc3bb
Will Power <github@willpoweredinc.co.uk>
2019-02-03 12:57:31 +0000

    improve offline handling

--- resources/assets/js/sw.js
+++ resources/assets/js/sw.js
@@ -5,6 +5,7 @@
  * https://developers.google.com/web/fundamentals/primers/service-workers/
  * https://github.com/jakearchibald/simple-serviceworker-tutorial/blob/gh-pages/sw.js
  * https://googlechrome.github.io/samples/service-worker/selective-caching/service-worker.js
+ * https://deanhume.com/create-a-really-really-simple-offline-page-using-service-workers/
  */
 
 (function() {
@@ -48,55 +49,67 @@
 
         // act on all requests
         self.addEventListener('fetch', function(ev) {
-            ev.respondWith(
-                self.caches.match(ev.request)
-                    .then(function(response) {
-                        if (response) {
-                            // 'Found ' + ev.request.url + ' in cache'
-                            return response;
-                        }
-
-                        // 'Network request for ' + ev.request.url
-                        return fetch(ev.request).then(function(response) {
-                            // could return custom 404 here
-
-                            /*
-                                if response is not an error
-                                    and response comes from same domain
-                                    and response has content-type header
-                                    and content-type says request was for image, css, or js
-                            */
-                            if (response.status < 400 &&
-                                ev.request.url.match(location.hostname) &&
-                                response.headers.has('Content-Type') &&
-                                (
-                                    response.headers.get('Content-Type').match(/^image\//i) ||
-                                    response.headers.get('Content-Type').match(/\/css$/i) ||
-                                    response.headers.get('Content-Type').match(/\/javascript$/i)
-                                )
-                            ) {
-                                // 'Caching ' + ev.request.url
-
-                                return self.caches.open(sitecachename).then(function(cache) {
-                                    cache.put(ev.request.url, response.clone());
-                                    return response;
-                                });
+            // if we're simply changing the page, don't bother consulting the service workers as we aren't caching pages
+            if (ev.request.mode === 'navigate' || (ev.request.method === 'GET' && ev.request.headers.get('accept').includes('text/html'))) {
+                ev.respondWith(
+                    fetch(ev.request).catch(function(error) {
+                        // 'Fetch failed; returning offline page instead.', error
+                        return caches.match(offlineurl);
+                    })
+                );
+            } else {
+                ev.respondWith(
+                    self.caches.match(ev.request)
+                        .then(function(response) {
+                            if (response) {
+                                // 'Found ' + ev.request.url + ' in cache'
+                                return response;
                             }
 
-                            // 'NOT caching ' + ev.request.url
-                            // out = {};
-                            // for (var pair of response.headers.entries()) {
-                            //     out[pair[0]] = pair[1];
-                            // }
-                            // console.table(out);
+                            // 'Network request for ' + ev.request.url
+                            return fetch(ev.request).then(function(response) {
+                                // could return custom 404 here
 
-                            return response;
-                        }).catch(function(error) {
-                            console.log('Fetch failed; returning offline page instead.', error);
-                            return caches.match(offlineurl);
-                        });
-                    })
-            );
+                                /*
+                                    if response is not an error
+                                        and response comes from same domain
+                                        and response has content-type header
+                                        and content-type says request was for image, css, or js
+                                */
+                                if (response.status < 400 &&
+                                    ev.request.url.match(location.hostname) &&
+                                    response.headers.has('Content-Type') &&
+                                    (
+                                        response.headers.get('Content-Type').match(/^image\//i) ||
+                                        response.headers.get('Content-Type').match(/\/css$/i) ||
+                                        response.headers.get('Content-Type').match(/\/javascript$/i)
+                                    )
+                                ) {
+                                    // 'Caching ' + ev.request.url
+
+                                    return self.caches.open(sitecachename).then(function(cache) {
+                                        cache.put(ev.request.url, response.clone());
+                                        return response;
+                                    });
+                                }
+
+                                // 'NOT caching ' + ev.request.url
+                                // out = {};
+                                // for (var pair of response.headers.entries()) {
+                                //     out[pair[0]] = pair[1];
+                                // }
+                                // console.table(out);
+
+                                return response;
+                            });
+                            // we're handling offline page navigation above,
+                            // this could do the same but whatever
+                            // }).catch(function(error) {
+                            //     console.log('Fetch failed; returning offline page instead.', error);
+                            //     return caches.match(offlineurl);
+                        })
+                );
+            }
         });
 
         // don't continue this closure
