2b0486dfc248e9fc8689d4e43bf07e6024534b7d
Will Power <github@willpoweredinc.co.uk>
2019-02-16 13:55:11 +0000

    handy

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,32 @@ if (! function_exists('usingsqlite')) {
         return (\DB::getDriverName() === 'sqlite');
     }
 }
+
+if (! function_exists('see_db_queries')) {
+    /**
+     * Shortcut to expose queries used by one or more commands
+     *
+     * Usage:
+     *
+     * see_db_queries(function() use ($object, $query) {
+     *     $object->save();
+     *     $test = $query->get();
+     * });
+     */
+    function see_db_queries($callable) {
+        if (! is_callable($callable)) {
+            throw new \Exception('Unable to see db queries: bad callable');
+        }
+
+        \DB::enableQueryLog();
+        $callable();
+
+        // if you're using chrome, it can't render dd output in the inspector so you want this
+        // if (request()->expectsJson()) {
+        //     var_dump(\DB::getQueryLog());
+        //     die();
+        // }
+
+        dd(\DB::getQueryLog());
+    }
+}
