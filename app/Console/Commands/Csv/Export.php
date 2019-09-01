6ca5469c8d6cc0c4fdad1c7a4ed698faccc8aaa3
William Hall <github@willpoweredinc.co.uk>
2019-09-01 21:12:35 +0100

    export and import codes as a rudimentary backup

---
+++ app/Console/Commands/Csv/Export.php
@@ -0,0 +1,98 @@
+<?php
+namespace App\Console\Commands\Csv;
+
+use Illuminate\Console\Command;
+use Validator;
+use App\Models\User;
+use App\Helpers\Encryption;
+
+class Export extends Command
+{
+    /**
+     * @inheritdoc
+     */
+    protected $signature = 'csv:export {destination? : the file to output to}';
+
+    /**
+     * @inheritdoc
+     */
+    protected $description = 'Export user tokens as CSV';
+
+    /**
+     * Authorise user and export token secrets
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->ask('Users email address?');
+
+        $password = $this->secret('Password for user?');
+
+        $user = compact('email', 'password');
+
+        $validator = Validator::make($user, User::getValidationRules('login'));
+
+        if ($validator->fails()) {
+            $this->info('Unable to login');
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return 1;
+        }
+
+        if (! auth()->validate($user)) {
+            $this->info('Unable to login');
+            $this->error('Unable to match details');
+            return 1;
+        }
+
+        session()->put('encryptionkey', Encryption::makeKey($user['password']));
+
+        $user = User::where('email', $user['email'])
+            ->with('tokens')
+            ->first();
+
+        $cache = '';
+
+        if (($line = $this->makeCSVLine(array('path', 'title', 'secret'))) === false) {
+            $this->info('Unable to build export');
+            return 1;
+        }
+
+        $cache .= $line . PHP_EOL;
+
+        foreach ($user->tokens as $token) {
+            $secret = $token->getDecryptedSecret();
+
+            if (($line = $this->makeCSVLine(array( $token->path, $token->title, $secret))) === false) {
+                $this->info('Problem building export');
+                return 1;
+            }
+
+            $cache .= $line . PHP_EOL;
+        }
+
+        file_put_contents($this->argument('destination') ?? 'output.csv', $cache);
+
+        $this->info('Done.');
+    }
+
+    /**
+     * Converts an array to a line of CSV-formatted data and returns
+     *
+     * @param array $fields list of fields
+     *
+     * @return bool|string CSV-formatted string or false on failure
+     */
+    private function makeCSVLine(array $fields)
+    {
+        $f = fopen('php://memory', 'r+');
+        if (fputcsv($f, $fields) === false) {
+            return false;
+        }
+        rewind($f);
+        $csv_line = stream_get_contents($f);
+        return rtrim($csv_line);
+    }
+}
---
+++ app/Console/Commands/Csv/Import.php
@@ -0,0 +1,94 @@
+<?php
+namespace App\Console\Commands\Csv;
+
+use Illuminate\Console\Command;
+use Validator;
+use App\Models\User;
+use App\Models\Token;
+use App\Helpers\Encryption;
+use DB;
+
+class Import extends Command
+{
+    /**
+     * @inheritdoc
+     */
+    protected $signature = 'csv:import {source? : the file to output to}';
+
+    /**
+     * @inheritdoc
+     */
+    protected $description = 'Import user tokens as CSV';
+
+    /**
+     * Authorise user and import token secrets
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->ask('Users email address?');
+
+        $password = $this->secret('Password for user?');
+
+        $user = compact('email', 'password');
+
+        $validator = Validator::make($user, User::getValidationRules('login'));
+
+        if ($validator->fails()) {
+            $this->info('Unable to login');
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return 1;
+        }
+
+        if (! auth()->validate($user)) {
+            $this->info('Unable to login');
+            $this->error('Unable to match details');
+            return 1;
+        }
+
+        session()->put('encryptionkey', Encryption::makeKey($user['password']));
+
+        $user = User::where('email', $user['email'])
+            ->with('tokens')
+            ->first();
+
+        // read the entire file into an array
+        $csv = array_map('str_getcsv', file($this->argument('source') ?? 'output.csv'));
+
+        // the first row of the CSV is the keys for the array
+        array_walk($csv, function(&$row) use ($csv) {
+            $row = array_combine($csv[0], $row);
+        });
+
+        // now remove the column headers
+        array_shift($csv);
+
+        DB::beginTransaction();
+
+        foreach ($csv as $newToken) {
+            $token = new Token(array(
+                'user_id' => $user->id,
+                'path' => $newToken['path'],
+                'title' => $newToken['title'],
+                'secret' => Encryption::encrypt($newToken['secret']),
+            ));
+
+            try {
+                $test = $token->getTOTPCode();
+            } catch (\Exception $e) {
+                DB::rollBack();
+
+                throw new \RuntimeException('Problem with secret for ' . $newToken['path']);
+            }
+
+            $token->save();
+        }
+
+        DB::commit();
+
+        $this->info('Done.');
+    }
+}
