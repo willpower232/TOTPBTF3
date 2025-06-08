2a20d8712d828ccec8a68fc35563c40a6aeee0ff
William Hall <github@willpoweredinc.co.uk>
2025-06-08 13:54:05 +0100

    🐛 need this obviously

---
+++ resources/views/vendor/login-link/loginLink.blade.php
@@ -0,0 +1,17 @@
+@env(config('login-link.allowed_environments'))
+    <form method="POST" action="{{ route('loginLinkLogin') }}">
+        @csrf
+
+        <input type="hidden" name="email" value="{{ $email }}">
+        <input type="hidden" name="key" value="{{ $key }}">
+        <input type="hidden" name="redirect_url" value="{{ $redirectUrl }}">
+        <input type="hidden" name="guard" value="{{ $guard }}">
+        <input type="hidden" name="user_attributes" value="{{ json_encode($userAttributes) }}">
+        <input type="hidden" name="user_model" value="{{ $userModel }}">
+
+        <!-- make this work with AddEncryptionKeyToSession -->
+        <input type="hidden" name="password" value="password">
+
+        @include('login-link::loginLinkButton')
+    </form>
+@endenv
